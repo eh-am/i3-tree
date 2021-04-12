da84b0086ddafb7512bac26b6fa8a5e2ef4ff271
eduardo aleixo <eduardoaleixomartins@gmail.com>
2021-04-12 17:13:36 -0300

    feat: implement cli usage with ffcli

--- main.go
+++ main.go
@@ -1,25 +1,48 @@
 package main
 
 import (
+	"context"
+	"flag"
+	"fmt"
 	"log"
 	"os"
 
 	cr "github.com/eh-am/i3-tree-viewer/conrenderer"
 	"github.com/eh-am/i3-tree-viewer/i3treeviewer"
 	"github.com/eh-am/i3-tree-viewer/treepruner"
+	"github.com/peterbourgon/ff/v3/ffcli"
 	"go.i3wm.org/i3/v4"
 )
 
 func main() {
-	tree, err := i3.GetTree()
-	if err != nil {
-		log.Fatal(err)
+	rootFs := flag.NewFlagSet("root", flag.ExitOnError)
+	pruneStratName := rootFs.String("prune-strat", treepruner.NonEmptyWsStrat.String(), "what to remove from the raw tree i3 returns. available: "+fmt.Sprintf("%s", treepruner.AvailableStrats))
+
+	root := &ffcli.Command{
+		Name:       "i3-tree-viewer",
+		ShortUsage: "i3-tree-viewer",
+		ShortHelp:  "Print a tree of non-empty workspaces to console",
+		FlagSet:    rootFs,
+		Exec: func(ctx context.Context, args []string) error {
+			tree, err := i3.GetTree()
+			if err != nil {
+				log.Fatal(err)
+			}
+
+			pruner, err := treepruner.NewStrat(*pruneStratName)
+			if err != nil {
+				return err
+			}
+
+			i3tv := i3treeviewer.NewI3TreeViewer(
+				pruner,
+				cr.NewConRenderer(os.Stdout, true),
+			)
+
+			i3tv.View(&tree)
+			return nil
+		},
 	}
 
-	i3tv := i3treeviewer.NewI3TreeViewer(
-		&treepruner.NeWsPruner{},
-		cr.NewConRenderer(os.Stdout, true),
-	)
-
-	i3tv.View(&tree)
+	root.ParseAndRun(context.Background(), os.Args[1:])
 }
